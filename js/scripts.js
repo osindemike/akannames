14d715dd0f8f65a82ce67670c91ebc824018b0f0
OsindeMike <michael.osinde@student.moringaschool.com>
2021-12-12 19:35:17 +0300

    add arrays for days, male and female names

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function detailsValidation(){
+function detailsValidation(formone){
   var details1= parseInt(formone.date.value, 10)
   if(details1<1 || details1>31){
     alert("Enter valid date of birth");
@@ -8,3 +8,6 @@ function detailsValidation(){
     alert("Enter a valid month")
   }
 }
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var namesMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var namesFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
