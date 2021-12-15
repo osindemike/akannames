3ba2b16c8c886b470124f535761c4ab054639cc2
OsindeMike <michael.osinde@student.moringaschool.com>
2021-12-15 14:22:22 +0300

    format layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,4 @@
 
-function validateEmpty(){
-  if(document.formone.date.value == "" || document.formone.date.value >31 || document.formone.date.value <=0){
-    alert("Enter valid date of birth");
-    }
-  else if(document.formone.month.value == "" || document.formone.month.value >12 || document.formone.month.value <=0){
-      alert("Enter valid month of birth");
-    }
-  else if(document.formone.year.value == "" || document.formone.year.value >2100 || document.formone.year.value <=1900){
-      alert("Enter valid year of birth");
-    }
-  else if (genderUser[0].checked == false && genderUser[1].checked ==false){
-      alert ("Kindly select one gender");
-    }
-}
 
 function getAkanname(){
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -23,7 +9,6 @@ var year= document.getElementById("year").value;
 var gender = document.querySelector('input[name="Gender"]:checked').value;
 var month= document.getElementById("month").value;
 var date= document.getElementById("date").value;
-var input= document.getElementById("date").value;
 var MM = parseInt(document.getElementById("month").value);
 var DD = parseInt(document.getElementById("date").value);
 var CC = parseInt(year.slice(0,2));
@@ -31,8 +16,8 @@ var YY = parseInt(year.slice(2,4));
 var d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
 d=d.toFixed(0);
 
-if (input='' && gender==''){
-  alert("enter values for akan");
+if (gender== ""){
+  alert("kindly select your gender");
 }
 else if (d ==1 && gender == "Male"){
   alert("Your akan name is " +namesMale[0]);
@@ -76,6 +61,21 @@ else if (d ==6 && gender == "Female"){
 else if (d ==7 && gender == "Female"){
   alert("Your akan name is" +namesFemale[6]);
 }
+}
+function validateEmpty(){
+  var genderUser = document.getElementsByName("gender");
+  if(document.formone.date.value == "" || document.formone.date.value >31 || document.formone.date.value <=0){
+    alert("Enter valid date of birth");
+    }
+  else if(document.formone.month.value == "" || document.formone.month.value >12 || document.formone.month.value <=0){
+      alert("Enter valid month of birth");
+    }
+  else if(document.formone.year.value == "" || document.formone.year.value >2100 || document.formone.year.value <=1900){
+      alert("Enter valid year of birth");
+    }
+  else if (genderUser[0].checked == false && genderUser[1].checked ==false){
+      alert ("Kindly select your gender");
+    }
 }
 
   
